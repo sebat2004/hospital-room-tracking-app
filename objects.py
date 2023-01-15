5cae8e534d33401dc6db080b4fad69bfee67d053
Sebastian <121001035+sebat2004@users.noreply.github.com>
2023-01-15 14:51:40 -0800

    Final Commit

--- objects.py
+++ objects.py
@@ -1,11 +1,8 @@
-from time import sleep
-
-
 class HouseKeeper:
-    def __init__(self, employee_id, name):
+    def __init__(self, employee_id, name, status="AVAILABLE"):
         self._employee_id = employee_id
         self._name = name
-        self._status = 'AVAILABLE'  # Can be marked: 'AVAILABLE', 'BUSY', 'ON_BREAK'
+        self._status = status  # Can be marked: 'AVAILABLE', 'BUSY', 'ON_BREAK'
         self._room_assigned = None
 
     def get_name(self):
@@ -23,26 +20,23 @@ class HouseKeeper:
 
     def set_room_assigned(self, room):
         self._room_assigned = room
-
+        room._house_keeper_assigned = self._employee_id
 
 
 class Room:
-    def __init__(self, room_number, room_status="CLEAN"):
-        self.room_number = room_number
-        self.room_status = room_status   # Can be marked 'CLEAN', 'DIRTY', 'FILTHY'
-        # self.patient_type = ""
+    def __init__(self, room_number, room_status="CLEAN", room_availability="AVAILABLE"):
+        self.room_number = room_number 
+        self.room_status = room_status # Can be marked 'CLEAN', 'DIRTY', 'FILTHY'
+        self.room_availability = room_availability # Either 'AVAILABLE', 'RF-CLEANING', 'CLEANING', or 'OCCUPIED'
         self.timer = 0
-        self.timer_on = False
         self._house_keeper_assigned = None
-    
-    # def usage_timer(self):
-    #     if self.patient_type == "Ambulatory":
-    #         self.timer = 60
-    #     elif self.patient_type == "Non-Ambulatory":
-    #         self.timer = 30
-    #     else:
-    #         self.timer = 45
-    #     return self.timer
+        
+    def usage_timer(self):
+        if self.room_status == "FILTHY":
+            self.timer = 20
+        elif self.room_status == "DIRTY":
+            self.timer = 10
+        return self.timer
 
     def get_room_number(self):
         return self.room_number
@@ -53,20 +47,13 @@ class Room:
 
     def get_update(self):
         """Returns ETA and Housekeeper who will be cleaning the room."""
-        if self._house_keeper_assigned:
-            return f"Room {self.room_number} will be cleaned by {self._house_keeper_assigned.get_name()} " \
-                   f"and be done in {self.timer} minutes."
-        else:
-            return "Room is cleaned and ready for the next patient."
+        return f"Room {self.room_number} is being cleaned by {self._house_keeper_assigned} and will be done in {self.timer} seconds."
 
     def get_assigned_house_keeper(self):
         return self._house_keeper_assigned
 
     def get_room_status(self):
         return self.room_status
-
-    def set_room_status(self, status):   # Can be marked 'CLEAN', 'DIRTY', 'FILTHY'
-        self.room_status = status
 
     def set_timer(self, time):
         self.timer = time
@@ -79,11 +66,19 @@ class Hospital:
 
     def add_house_keeper(self, house_keeper):
         """Adds a HouseKeeper object to the Hospital's house_keepers list."""
-        self._house_keepers.append(house_keeper)
+        if len(self._house_keepers) == 0:
+            return self._house_keepers.append(house_keeper)     
+        for keeper in self._house_keepers:
+            if house_keeper._employee_id == keeper._employee_id:
+                return "ID in use"
+        self._house_keepers.append(house_keeper)  
+        print("worked")
+        return f"Housekeeper {house_keeper._employee_id} added"     
+        
 
     def remove_house_keeper(self, house_keeper):
         """Removes a HouseKeeper object from the Hospital's house_keepers list."""
-        self._house_keepers.remove(house_keeper)
+        return self._house_keepers.remove(house_keeper)
 
     def add_room(self, room):
         """Adds a Room object to the hospital."""
@@ -95,46 +90,40 @@ class Hospital:
 
     def request_room_cleaning(self, room):
         """Finds an available house_keeper and sets their room assignment"""
-        if room.get_room_status() == "CLEAN":
-            return "Room has already been cleaned!"
         for house_keeper in self._house_keepers:
-            if house_keeper.get_employee_status() == 'AVAILABLE':
+            if house_keeper._status == 'AVAILABLE':
                 room.set_house_keeper_assigned(house_keeper)
-                house_keeper.set_room_assigned(room)
-                house_keeper.set_employee_status('BUSY')
+                room.room_availability == "RF-CLEANING"
+                house_keeper.set_room_assigned = room
+                house_keeper.set_employee_status = 'BUSY'
                 return "Room Assignment Successful!"
-        else:
-            return "There are no available house keepers at this time"  # Figure out a queue system for if all keepers are busy that gives time frame of arrival of next HK
+        return "There are no available house keepers"  # Figure out a queue system for if all keepers are busy that gives time frame of arrival of next HK
 
     def cleaning_beginning(self, room_number, time=0):
         """Gives estimated time of completion, time chosen based on room condition if time is not entered."""
         for room in self._rooms:
-            if room.get_room_number() == room_number:
+            if room.room_number == room_number and room.room_availability == "RF-CLEANING":
                 if time == 0:
-                    if room.get_room_status() == 'CLEAN':
-                        return "Room is already cleaned!"
-                    elif room.get_room_status() == 'DIRTY':
-                        room.set_timer(30)
+                    room.room_availability = 'CLEANING'
+                    if room.get_room_status() == 'DIRTY':
+                        room.timer = 10
+                        return
                     elif room.get_room_status() == 'FILTHY':
-                        room.set_timer(60)
+                        room.timer = 10
+                        return
                 elif time != 0:
-                    room.set_timer(time)
+                    room.timer = time
+        return "Room is being cleaned"
 
-    def cleaning_complete(self, room_number):
-        """Sets room as 'CLEAN' and house keeper as 'AVAILABLE', removes housekeeper assignment """
-        for room in self._rooms:
-            if room.get_room_number() == room_number:
-                room.set_room_status('CLEAN')
-                house_keeper = room.get_assigned_house_keeper()
-                room.set_house_keeper_assigned(None)
-                house_keeper.set_room_assigned(None)
-                house_keeper.set_employee_status('AVAILABLE')
-                room.set_timer(0)
-
-
-
-
-
+    def cleaning_complete(self, room):
+        """Sets room as 'CLEAN' and house keeper as 'AVAILABLE', removes house keeper assignment """
+        room.room_status = 'CLEAN'
+        room.room_availability = 'AVAILABLE'
+        house_keeper = room.get_assigned_house_keeper()
+        room.set_house_keeper_assigned(None)
+        house_keeper._room_assigned = None
+        house_keeper._status = 'AVAILABLE'
+        room.set_timer(0
 
 if __name__ == '__main__':
 
