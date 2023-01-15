204fc63bbbfe216daaa2cac70bf08f8722de3566
sebat2004 <sebat2004@gmail.com>
2023-01-14 18:39:10 -0800

    Made GUI Functional

--- test-gui.py
+++ test-gui.py
@@ -2,6 +2,9 @@ import tkinter
 import tkinter.messagebox
 import customtkinter
 
+# below is not yet functional, meant to import room class
+# import functions as hospital
+
 customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
@@ -19,71 +22,98 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure((2), weight=0)
         self.grid_rowconfigure((0), weight=1)
 
-        def home_page(self):
-            home_frame = customtkinter.CTkFrame(self)
+        # text entry vars
+        self.create_room_num = customtkinter.StringVar()
+        
+        # list of all room classes
+        self.rooms = []
 
-            # create navbar
-            nav_bar("home_button")
+    # page creation functions
+    def home_page(self):
+        home_frame = customtkinter.CTkFrame(self)
 
-            # contents of home page
-            label = customtkinter.CTkLabel(home_frame, text="Home Page")
-            label.grid(row=0, column=1, padx=20, pady=20)
+        # create navbar
+        self.nav_bar("home")
 
-            home_frame.grid(row=0, column=1, padx=20, pady=20)
+        # contents of home page
+        label = customtkinter.CTkLabel(home_frame, text="Home Page")
+        label.grid(row=0, column=1, padx=20, pady=20)
 
-        # Navigation bar (left side of screen)
-        def nav_bar(gray_button):
-            self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
-            self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
-            self.sidebar_frame.grid_rowconfigure(4, weight=1)
-            
-            self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="RoomTracker", font=customtkinter.CTkFont(size=20, weight="bold"))
-            self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 15))
+        home_frame.grid(row=0, column=1, padx=20, pady=20)
 
-            self.home_button = customtkinter.CTkButton(self.sidebar_frame, text="Home", command=lambda: home_page)
-            self.home_button.grid(row=1, column=0, padx=20, pady=10)
+    def patient_page(self):
+        patient_frame = customtkinter.CTkFrame(self)
 
-            self.patient_button = customtkinter.CTkButton(self.sidebar_frame, text="Add Patient")
-            self.patient_button.grid(row=2, column=0, padx=20, pady=10)
+        # create navbar
+        self.nav_bar("patient")
 
-            self.status_button = customtkinter.CTkButton(self.sidebar_frame, text="Room Status View")
-            self.status_button.grid(row=3, column=0, padx=20, pady=10)
+        # create add patient tab
+        self.patient_tab = customtkinter.CTkTabview(self, width=250)
+        self.patient_tab.grid(row=0, column=1, padx=(20, 20), pady=(15, 20), sticky="nsew")
+        self.patient_tab.add("Add Patient")
+        self.patient_tab.tab("Add Patient").grid_columnconfigure(0, weight=1)
 
-            self.gray_button.configure(fg_color="7F8590")
+        self.dropdown_menu = customtkinter.CTkComboBox(self.patient_tab.tab("Add Patient"),
+                                                    values=["Ambulatory Patient", "Unknown Type", "Etc..."], width=170)
+        self.dropdown_menu.grid(row=0, column=0, padx=20, pady=(100, 10))
 
-        def patient_page(self):
-            home_frame = customtkinter.CTkFrame(self)
+        self.assign_room_entry = customtkinter.CTkEntry(self.patient_tab.tab("Add Patient"), textvariable=self.create_room_num)
+        self.assign_room_entry.grid(row=1, column=0, padx=20, pady=20)
+        
+        self.create_room_button = customtkinter.CTkButton(self.patient_tab.tab("Add Patient"), text="Create Room",
+                                                        command=self.create_room)
+        self.create_room_button.grid(row=2, column=0, padx=20, pady=(15, 10))
 
-            # create tabview
-            self.tabview = customtkinter.CTkTabview(self, width=250)
-            self.tabview.grid(row=0, column=1, padx=(20, 20), pady=(15, 20), sticky="nsew")
-            self.tabview.add("Add Patient")
-            self.tabview.add("View Rooms")
-            self.tabview.tab("Add Patient").grid_columnconfigure(0, weight=1)
-            self.tabview.tab("View Rooms").grid_columnconfigure(0, weight=1)
+        # dropdown default text
+        self.dropdown_menu.set("Select Patient Type")
+    
+    def status_page(self):
+        status_frame = customtkinter.CTkFrame(self)
 
-            self.combobox_1 = customtkinter.CTkComboBox(self.tabview.tab("Add Patient"),
-                                                        values=["Ambulatory Patient", "Unknown Type", "Etc..."], width=170)
-            self.combobox_1.grid(row=0, column=0, padx=20, pady=(100, 10))
-            
-            self.string_input_button = customtkinter.CTkButton(self.tabview.tab("Add Patient"), text="Create Room",
-                                                            command=self.create_room_event)
-            self.string_input_button.grid(row=1, column=0, padx=20, pady=(15, 10))
+        self.nav_bar("status")
 
-            self.label_tab_2 = customtkinter.CTkLabel(self.tabview.tab("View Rooms"))
-            self.label_tab_2.grid(row=0, column=0, padx=20, pady=20)
+        # create visual progress bar for each room
 
-        # set default values
-        self.combobox_1.set("Select Patient Type")
+
+    # Navigation bar (left side of screen)
+    def nav_bar(self, gray_button):
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(4, weight=1)
+        
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="RoomTracker", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 15))
+
+        self.home_button = customtkinter.CTkButton(self.sidebar_frame, text="Home", command= lambda:[self.delete_pages(), self.home_page()])
+        self.home_button.grid(row=1, column=0, padx=20, pady=10)
+
+        self.patient_button = customtkinter.CTkButton(self.sidebar_frame, text="Add Patient", command= lambda:[self.delete_pages(), self.patient_page()])
+        self.patient_button.grid(row=2, column=0, padx=20, pady=10)
+
+        self.status_button = customtkinter.CTkButton(self.sidebar_frame, text="Room Status View", command= lambda:[self.delete_pages(), self.status_page()])
+        self.status_button.grid(row=3, column=0, padx=20, pady=10)
+        if gray_button == "home":
+            self.home_button.configure(fg_color="#7F8590")
+        elif gray_button == "patient":
+            self.patient_button.configure(fg_color="#7F8590")
+        else:
+            self.status_button.configure(fg_color="#7F8590")
+
+    def delete_pages(self):
+        for frame in self.winfo_children():
+            frame.destroy()
 
     def open_input_dialog_event(self):
-        dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
-        print("CTkInputDialog:", dialog.get_input())
+        # dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
+        # print("CTkInputDialog:", dialog.get_input())
+        pass
 
-    def create_room_event(self):
-        print("sidebar_button click")
+    def create_room(self):
+        # creates a room object when called
+        pass
 
 
 if __name__ == "__main__":
     app = App()
+    app.home_page()
     app.mainloop()
