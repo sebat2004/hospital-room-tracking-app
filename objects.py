4eb20987c84e2343468cadd973a30b31e54d40eb
VegetableSchmedly <iericdaly@gmail.com>
2023-01-15 14:59:33 -0500

    Version 1.0 of Objects Tested briefly, still needs a thorough unittest file.

--- objects.py
+++ objects.py
@@ -25,6 +25,7 @@ class HouseKeeper:
         self._room_assigned = room
 
 
+
 class Room:
     def __init__(self, room_number, room_status="CLEAN"):
         self.room_number = room_number
@@ -52,14 +53,20 @@ class Room:
 
     def get_update(self):
         """Returns ETA and Housekeeper who will be cleaning the room."""
-        return f"Room {self.room_number} will be cleaned by {self._house_keeper_assigned} " \
-               f"and be done in {self.timer} minutes."
+        if self._house_keeper_assigned:
+            return f"Room {self.room_number} will be cleaned by {self._house_keeper_assigned.get_name()} " \
+                   f"and be done in {self.timer} minutes."
+        else:
+            return "Room is cleaned and ready for the next patient."
 
     def get_assigned_house_keeper(self):
         return self._house_keeper_assigned
 
     def get_room_status(self):
         return self.room_status
+
+    def set_room_status(self, status):   # Can be marked 'CLEAN', 'DIRTY', 'FILTHY'
+        self.room_status = status
 
     def set_timer(self, time):
         self.timer = time
@@ -88,11 +95,13 @@ class Hospital:
 
     def request_room_cleaning(self, room):
         """Finds an available house_keeper and sets their room assignment"""
+        if room.get_room_status() == "CLEAN":
+            return "Room has already been cleaned!"
         for house_keeper in self._house_keepers:
-            if house_keeper.get_employee_status == 'AVAILABLE':
+            if house_keeper.get_employee_status() == 'AVAILABLE':
                 room.set_house_keeper_assigned(house_keeper)
-                house_keeper.set_room_assigned = room
-                house_keeper.set_employee_status = 'BUSY'
+                house_keeper.set_room_assigned(room)
+                house_keeper.set_employee_status('BUSY')
                 return "Room Assignment Successful!"
         else:
             return "There are no available house keepers at this time"  # Figure out a queue system for if all keepers are busy that gives time frame of arrival of next HK
@@ -102,28 +111,56 @@ class Hospital:
         for room in self._rooms:
             if room.get_room_number() == room_number:
                 if time == 0:
-                    if room.get_room_status() == 'DIRTY':
+                    if room.get_room_status() == 'CLEAN':
+                        return "Room is already cleaned!"
+                    elif room.get_room_status() == 'DIRTY':
                         room.set_timer(30)
                     elif room.get_room_status() == 'FILTHY':
                         room.set_timer(60)
                 elif time != 0:
                     room.set_timer(time)
 
-    def cleaning_complete(self, room):
-        """Sets room as 'CLEAN' and house keeper as 'AVAILABLE', removes house keeper assignment """
-        room.room_status = 'CLEAN'
-        house_keeper = room.get_assigned_house_keeper()
-        room.set_house_keeper_assigned(None)
-        house_keeper.set_room_assigned(None)
-        house_keeper.set_employee_status('AVAILABLE')
-        room.set_timer(0)
+    def cleaning_complete(self, room_number):
+        """Sets room as 'CLEAN' and house keeper as 'AVAILABLE', removes housekeeper assignment """
+        for room in self._rooms:
+            if room.get_room_number() == room_number:
+                room.set_room_status('CLEAN')
+                house_keeper = room.get_assigned_house_keeper()
+                room.set_house_keeper_assigned(None)
+                house_keeper.set_room_assigned(None)
+                house_keeper.set_employee_status('AVAILABLE')
+                room.set_timer(0)
 
 
 
 
 
 
+if __name__ == '__main__':
 
 
-
-
+    # hospital = Hospital()
+    # r1 = Room(1)
+    # r2 = Room(2)
+    # r3 = Room(3)
+    # hospital.add_room(r1)
+    # hospital.add_room(r2)
+    # hospital.add_room(r3)
+    #
+    # hk1 = HouseKeeper(123, 'Eric')
+    # hk2 = HouseKeeper(456, 'Sebastian')
+    # hk3 = HouseKeeper(789, 'Hannah')
+    # hospital.add_house_keeper(hk1)
+    # hospital.add_house_keeper(hk2)
+    # hospital.add_house_keeper(hk3)
+    #
+    # r1.set_room_status('FILTHY')
+    # print(hospital.request_room_cleaning(r1))
+    #
+    # hospital.cleaning_beginning(1)
+    # print(r1.get_update())
+    #
+    # hospital.cleaning_complete(1)
+    # print(r1.get_update())
+    #
+    # print(hospital.request_room_cleaning(r1))
